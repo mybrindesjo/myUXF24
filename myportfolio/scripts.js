6174d86a7f7673de8bf82d66ebb3889f27d51eb2
My <my.brindesjo@gmail.com>
2025-01-15 11:46:12 +0100

    New changes 15-01-2025

--- myportfolio/scripts.js
+++ myportfolio/scripts.js
@@ -4,11 +4,16 @@ document.addEventListener("DOMContentLoaded", function() {
     toggleButtons.forEach(button => {
         button.addEventListener("click", function() {
             const description = this.nextElementSibling;
+            const infoCard = this.closest(".info-card");
             if (description.style.display === "none" || description.style.display === "") {
                 description.style.display = "block";
+                infoCard.style.height = "auto";
+                infoCard.style.maxHeight = "none"; // Allow the card to expand
                 this.textContent = "Visa mindre";
             } else {
                 description.style.display = "none";
+                infoCard.style.height = "auto";
+                infoCard.style.maxHeight = ""; // Reset to default height
                 this.textContent = "Visa mer";
             }
         });
